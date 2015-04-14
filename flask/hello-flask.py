b78e908abf43149acf37c24440dd2980f0689589
tscloud <thomas.s.cloud@gmail.com>
2015-04-14 00:30:26 -0400

    Initial commit of flask web interface.

---
+++ flask/hello-flask.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True)
