40180131cabbafce21869f8960ed72c5934c6c2e
tscloud <thomas.s.cloud@gmail.com>
2015-04-24 19:22:52 -0400

    More init service stuff.

--- service/startup.sh
+++ service/startup.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-$PIGOSEMIPRO_DIR/PiGoSemiPro.py --path=$PIGOSEMIPRO_DIR --config=$PIGOSEMIPRO_DIR/.config_pigosemipro.cfg > $PIGOSEMIPRO_DIR/service.log 2>&1
+$PIGOSEMIPRO_DIR/PiGoSemiPro.py --path=$PIGOSEMIPRO_DIR --config=$PIGOSEMIPRO_DIR/.config_pigosemipro.cfg > $PIGOSEMIPRO_DIR/service/service.log 2>&1
